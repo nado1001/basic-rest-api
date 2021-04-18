4b107b1fe7dc4f3edd89ee58dea0b29036bd458f
take-tech1001 <tk.tech1001@gmail.com>
2021-04-19 02:02:32 +0900

    Added create user api

--- app/app.js
+++ app/app.js
@@ -3,7 +3,13 @@ const app = express()
 const sqlite3 = require('sqlite3')
 const dbPath = 'app/db/database.sqlite3'
 const path = require('path')
+const bodyParser = require('body-parser')
 
+// リクエストのbodyをパースする設定
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+// publicディレクトリを静的ファイル群のルートディレクトリとして設定
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Get all users
@@ -38,6 +44,33 @@ app.get('/api/v1/search', (req, res) => {
     res.json(rows)
   })
 
+  db.close()
+})
+
+// Create a new user
+app.post('/api/v1/users', async (req, res) => {
+  // Connect database
+  const db = new sqlite3.Database(dbPath)
+
+  const name = req.body.name
+  const profile = req.body.profile ? req.body.profile : ""
+  const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : ""
+
+  const run = async (sql) => {
+    return new Promise((resolve, reject) => {
+      db.run(sql, (err) => {
+        if (err) {
+          res.status(500).send(err)
+          return reject()
+        } else {
+          res.json({message: "新規ユーザーを作成しました！"})
+          return resolve();
+        }
+      })
+    })
+  }
+
+  await run(`INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}", "${profile}", "${dateOfBirth}")`)
   db.close()
 })
 
